f667e6974e7abc53757cba1c6985cbb3139bed84
Elias Gomes <eliasgomesduraes2008@gmail.com>
2026-03-13 21:33:09 -0300

    Correção.

--- 10_loopwhile.php
+++ 10_loopwhile.php
@@ -7,25 +7,6 @@
         </head>
         <body>
             <?php
-                $i = 1;
-                while($i <=5){
-                    echo "$i<br>";
-                    $i++;
-                }
-            ?>
-        </body>
-    </html>
- 
-10 
-<!DOCTYPE html>
-    <html lang="pt-br">
- 
-        <head>
-            <meta charset="UTF-8">
-            <title>Curso de PHP</title>
-        </head>
-        <body>
-            <?php
                 $num_par = 0;
                 while($num_par <= 20){
                    
